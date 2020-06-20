df3401b030711c0362e57857ede5d3e7fc367f48
dtc103 <jan_kerner@gmx.de>
2020-06-21 00:46:03 +0200

    .

--- app.py
+++ app.py
@@ -6,15 +6,30 @@ import database_ops
 from dotenv import load_dotenv
 import os
 
+import sys
+
+import hashlib
+
+#if len(sys.argv) != 2:
+#    print("Called script wrong")
+#    exit()
+
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 USERNAME = os.getenv("DB_USER")
 PW = os.getenv("DB_PW")
+print(PW)
 
-# later add these
-db_cursor = database_ops.open_database("localhost", USERNAME, PW, "pictures")
+#later add these
+#db_cursor = database_ops.open_database("localhost", USERNAME, PW, "pictures")
 
 bot = commands.Bot(command_prefix="!")
-bot.add_cog(sketbot.IconRandomizerCog(bot, db_cursor))
+bot.add_cog(sketbot.IconRandomizerCog(bot))
 
 bot.run(TOKEN)
+
+
+#if finished -> use hash to check for password
+#h = hashlib.sha512()
+#h.update(PW.encode("utf-8"))
+#print(h.hexdigest())
